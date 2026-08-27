9b86ecf4f361b50b82fb77973b4000b4f5f6cb32
mmhhjdev <seyedmahanhejrati@gmail.com>
2026-08-27 16:39:31 +0330

    edit for deploing

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/', // اصلاح شد تا روی دامنه اختصاصی بدون خطا لود شود
+    // اگر سایت مستقیماً روی دامنه اصلی بالا می آید، '/' درست است.
+    // اما اگر روی گیت‌هاب پیجز به صورت ساب‌پوشه ارور داد، آن را روی '/parisima-clinic/' قرار دهید.
+    base: '/parisima-clinic/', 
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
